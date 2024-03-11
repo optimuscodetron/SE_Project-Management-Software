c19c49b3d733ca65c162345e16a8681b2558ca4b
Vavadiya Harsh <113345798+HarshVavadiya2@users.noreply.github.com>
2024-03-11 20:23:32 +0530

    Created a page of team members details and also modified the project details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,74 +1,76 @@
-import React from 'react';
+import React from "react";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Router, Redirect } from '@reach/router';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { gapi } from 'gapi-script';
-import Otp from './Authantication/Forgetpassword/otp';
+import { Router, Redirect } from "@reach/router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { gapi } from "gapi-script";
+import Otp from "./Authantication/Forgetpassword/otp";
 
-import Login from './Authantication/views/Login';
-import Registration from './Authantication/views/Registration';
-import Email from './Authantication/Forgetpassword/email';
+import Login from "./Authantication/views/Login";
+import Registration from "./Authantication/views/Registration";
+import Email from "./Authantication/Forgetpassword/email";
 import loginbutton from "./Authantication/views/Login";
 
-import Board from './Project/ProjectBoard/Board';
-import Settings from './Project/ProjectSettings/Settings';
-import Workspace from './Workspace'
+import Board from "./Project/ProjectBoard/Board";
+import Settings from "./Project/ProjectSettings/Settings";
+import Workspace from "./Workspace";
 import WorkPage from "./Create_workspace/Components/WorkPage";
+import Members from "./Project/ProjectSettings/Members";
 
-
-const clientid = "495965121219-65gvv679mrctt1ksda4048jtmu4r1to4.apps.googleusercontent.com";
-
+const clientid =
+  "495965121219-65gvv679mrctt1ksda4048jtmu4r1to4.apps.googleusercontent.com";
 
 function App() {
-    useEffect(() => {
-        function start() {
-            gapi.client.init({
-                clientid: clientid,
-                scope: ""
-            })
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientid: clientid,
+        scope: "",
+      });
+    }
+    gapi.load("client:auth2", start);
+  });
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="App">
+      <loginbutton />
+      <BrowserRouter>
+        {/* <Redirect from="/" to="/register" noThrow /> */}
+        {/* <Route path='/user/otp' element={<Otp />} /> */}
 
-        };
-        gapi.load('client:auth2', start);
-    })
-    const onSubmit = (e) => {
-        e.preventDefault();
-    };
-    return (
-        <div className="App">
-            <loginbutton />
-            <BrowserRouter>
-                {/* <Redirect from="/" to="/register" noThrow /> */}
-                {/* <Route path='/user/otp' element={<Otp />} /> */}
+        {/* <Registration path="/register" /> */}
+        {/* <Email path="/email" /> */}
+        {/* <Login path="/login" /> */}
+        {/* <Main path="/home" onSubmit={onSubmit} /> */}
+        {/* <Main path="/home/geer/:id" onSubmit={onSubmit} /> */}
+        {/* <NewUser path="/welcome" /> */}
+        <Routes>
+          {/* <Route path="/authenticate" component={Authenticate} /> */}
+          {/* <Route exact path="/register" element={< />} /> */}
+          <Route path="/register" element={<Registration />} />
+          <Route path="/email" element={<Email />} />
+          <Route path="/login" element={<Login />} />
 
-                {/* <Registration path="/register" /> */}
-                {/* <Email path="/email" /> */}
-                {/* <Login path="/login" /> */}
-                {/* <Main path="/home" onSubmit={onSubmit} /> */}
-                {/* <Main path="/home/geer/:id" onSubmit={onSubmit} /> */}
-                {/* <NewUser path="/welcome" /> */}
-                <Routes>
-                    {/* <Route path="/authenticate" component={Authenticate} /> */}
-                    {/* <Route exact path="/register" element={< />} /> */}
-                    <Route  path="/register" element={<Registration />} />
-                    <Route  path="/email" element={<Email />} />
-                    <Route  path="/login" element={<Login />} />
+          {/* by ayush */}
+          <Route path="/create_workspace" element={<WorkPage />}>
+            {" "}
+          </Route>
 
-                    {/* by ayush */}
-                    <Route path="/create_workspace" element={<WorkPage/>}> </Route>
-                   
-
-
-
-                    <Route  path="/workspace" element={<Workspace />} />
-                    <Route  path="/workspace/project/board" element={<Board />} />
-                    <Route  path="/workspace/project/settings" element={<Settings />} />
-                    {/* <Route component={PageError} /> */}
-                </Routes>
-            </BrowserRouter>
-        </div >
-    );
+          <Route path="/workspace" element={<Workspace />} />
+          <Route path="/workspace/project/board" element={<Board />} />
+          <Route path="/workspace/project/settings" element={<Settings />} />
+          <Route
+            path="/workspace/project/settings/members"
+            element={<Members />}
+          />
+          {/* <Route component={PageError} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
