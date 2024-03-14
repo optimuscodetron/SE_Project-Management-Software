d7e9c6c255bb4787d42f3d6821d89aec9d8a01fd
Vavadiya Harsh <113345798+HarshVavadiya2@users.noreply.github.com>
2024-03-15 03:35:38 +0530

    Made some final changes of the page
    Co-Authored-By: kamble268 <116895162+kamble268@users.noreply.github.com>
    Co-Authored-By: SUSHIL KUMAR  <115349532+mrsushilkumar@users.noreply.github.com>

--- client/src/Project/ProjectBoard/IssueInfo/Components/RightBar.jsx
+++ client/src/Project/ProjectBoard/IssueInfo/Components/RightBar.jsx
@@ -4,20 +4,20 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function RightBar() {
   const [status, setStatus] = useState("1");
-  const [cooldownDuration, setPriority] = useState("1");
+  const [priority, setPriority] = useState("1");
+  const [cycle, setCycle] = useState("1");
   const [dueDate, setDueDate] = useState(null);
   const [assigne, setAssigne] = useState("Harsh");
-  const [cycle, setCycle] = useState("Cycle 2");
   const [project, setProject] = useState("Project_name");
   return (
     <div
-      className="bg-black w-[38vw] flex flex-col overflow-auto text-gray-900 p-3"
+      className="bg-gray-900 w-[38vw] flex flex-col overflow-auto text-white p-3"
       style={{
         scrollbarWidth: "thin",
         scrollbarColor: "rgba(0,0,0,0) rgba(0,0,0,0)",
       }}
     >
-      <div className="mt-10 mb-4 flex justify-start border-b border-gray-600 pb-4">
+      <div className="mt-10 mb-4 flex justify-start ">
         <div className="text-white w-[20%] mr-3">Status</div>
         <select
           value={status}
@@ -52,71 +52,99 @@ function RightBar() {
             Duplicate{"   "}
             {status === "7" && <span className="ml-2">&#10003;</span>}
           </option>
-
         </select>
       </div>
 
-
-      <div className="mb-4 flex justify-start border-b border-gray-600 pb-4">
+      <div className="mb-4 flex justify-start">
         <div className="text-white w-[20%] mr-3">Priority</div>
         <select
-          value={cooldownDuration}
+          value={priority}
           onChange={(e) => setPriority(e.target.value)}
           className="block appearance-none w-[30%]  h-7  bg-[rgb(15,19,29)] border border-gray-400 hover:border-gray-500 px-2 rounded shadow leading-tight focus:outline-none focus:border-blue-500 text-white overflow-x-hidden"
         >
           <option value="1">
             No priority{"   "}
-            {cooldownDuration === "1" && <span className="ml-2">&#10003;</span>}
+            {priority === "1" && <span className="ml-2">&#10003;</span>}
           </option>
           <option value="2">
             Urgent{"   "}
-            {cooldownDuration === "2" && <span className="ml-2">&#10003;</span>}
+            {priority === "2" && <span className="ml-2">&#10003;</span>}
           </option>
           <option value="3">
             High{"   "}
-            {cooldownDuration === "3" && <span className="ml-2">&#10003;</span>}
+            {priority === "3" && <span className="ml-2">&#10003;</span>}
           </option>
           <option value="4">
             Medium{"   "}
-            {cooldownDuration === "4" && <span className="ml-2">&#10003;</span>}
+            {priority === "4" && <span className="ml-2">&#10003;</span>}
           </option>
           <option value="5">
             Low{"   "}
-            {cooldownDuration === "5" && <span className="ml-2">&#10003;</span>}
+            {priority === "5" && <span className="ml-2">&#10003;</span>}
           </option>
-
         </select>
       </div>
 
-      <div className="mb-4 flex justify-start border-b border-gray-600 pb-4">
-        <div className="text-white w-[20%] mr-3">
-          Assigne
-        </div>
+      <div className="mb-4 flex justify-start">
+        <div className="text-white w-[20%] mr-3">Assigne</div>
         <div className="block appearance-none w-[30%]  h-7  bg-[rgb(15,19,29)] border border-gray-400 hover:border-gray-500 px-2 rounded shadow leading-tight focus:outline-none focus:border-blue-500 text-white">
           {assigne}
         </div>
       </div>
 
-      <div className="mb-4 flex justify-start border-b border-gray-600 pb-4">
-        <div className="text-white w-[20%] mr-3">
-          Cycle
-        </div>
-        <div className="block appearance-none w-[30%]  h-7  bg-[rgb(15,19,29)] border border-gray-400 hover:border-gray-500 px-2 rounded shadow leading-tight focus:outline-none focus:border-blue-500 text-white overflow-x-hidden">
-          {cycle}
-        </div>
+      <div className="mb-4 flex justify-start">
+        <div className="text-white w-[20%] mr-3">Cycle</div>
+        <select
+          value={cycle}
+          onChange={(e) => setCycle(e.target.value)}
+          className="block appearance-none w-[30%]  h-7  bg-[rgb(15,19,29)] border border-gray-400 hover:border-gray-500 px-2 rounded shadow leading-tight focus:outline-none focus:border-blue-500 text-white overflow-x-hidden"
+        >
+          <option value="1">
+            Cycle 1{"   "}
+            {cycle === "1" && <span className="ml-2">&#10003;</span>}
+          </option>
+          <option value="2">
+            Cycle 2{"   "}
+            {cycle === "2" && <span className="ml-2">&#10003;</span>}
+          </option>
+          <option value="3">
+            Cycle 3{"   "}
+            {cycle === "3" && <span className="ml-2">&#10003;</span>}
+          </option>
+          <option value="4">
+            Cycle 4{"   "}
+            {cycle === "4" && <span className="ml-2">&#10003;</span>}
+          </option>
+          <option value="5">
+            Cycle 5{"   "}
+            {cycle === "5" && <span className="ml-2">&#10003;</span>}
+          </option>
+          <option value="6">
+            Cycle 6{"   "}
+            {cycle === "6" && <span className="ml-2">&#10003;</span>}
+          </option>
+          <option value="7">
+            Cycle 7{"   "}
+            {cycle === "7" && <span className="ml-2">&#10003;</span>}
+          </option>
+          <option value="8">
+            Cycle 8{"   "}
+            {cycle === "8" && <span className="ml-2">&#10003;</span>}
+          </option>
+          {/* Add duration options */}
+        </select>
       </div>
 
-      <div className="mb-4 flex justify-start border-b border-gray-600 pb-4">
-        <div className="text-white w-[20%] mr-3">
-          Project
-        </div>
+      <hr class="w-[95%] h-1 mx-auto mb-5 mt-3 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-400"></hr>
+
+      <div className="mb-4 flex justify-start">
+        <div className="text-white w-[20%] mr-3">Project</div>
         <div className="block appearance-none w-[30%] h-7 bg-[rgb(15,19,29)] border border-gray-400 hover:border-gray-500 px-2 rounded shadow leading-tight focus:outline-none focus:border-blue-500 text-white overflow-x-hidden">
           {project}
         </div>
       </div>
 
-
-      <div className="mb-4 flex justify-start border-b border-gray-600 pb-4">
+      <div className="mb-4 flex justify-start">
         <div className="text-white w-[20%] mr-3">Due date</div>
         <DatePicker
           selected={dueDate}
--- client/src/Project/ProjectBoard/IssueInfo/Components/TitleDescrip.jsx
+++ client/src/Project/ProjectBoard/IssueInfo/Components/TitleDescrip.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TitleDescrip() {
-  const titleZ = "Issue Title ";
-  const titleD = " Description";
+  const [titleZ, setTitleZ] = useState("Initial Title");
+  const [titleD, setTitleD] = useState("Description");
+
+  const handleTitleChange = (event) => {
+    setTitleZ(event.target.innerText);
+  };
+  const handleDescriptionChange = (event) => {
+    setTitleD(event.target.innerText);
+  };
+
   return (
-    <div className="h-auto p-3 m-3 mb-0">
-      <h1 className="text-3xl tracking-wide font-semibold mb-10">{titleZ}</h1>
-      <p className=" tracking-wide font-semibold mb-10">{titleD}</p>
+    <div className="h-auto p-3 m-3 mb-0 ">
+      <h1
+        className="text-3xl tracking-wide mb-[3vh] font-semibold"
+        contentEditable="true"
+        onBlur={handleTitleChange}
+      >
+        {titleZ}
+      </h1>
+      <p
+        className=" tracking-wide text-wrap  font-semibold min-h-[10vh] border-0"
+        contentEditable="true"
+        onBlur={handleDescriptionChange}
+      >
+        {titleD}
+      </p>
     </div>
   );
 }
