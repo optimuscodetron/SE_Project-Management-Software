c9c8e4da88a715143aaa2a43e76bd22c05f8eeb8
Patel-Het03 <hetrpatel03@gmail.com>
2024-03-15 02:34:04 +0530

    Made changes some sidebar

--- client/src/Project/ProjectBoard/Componets/IssueStage.jsx
+++ client/src/Project/ProjectBoard/Componets/IssueStage.jsx
@@ -10,7 +10,7 @@ const style = {
 export default function IssueStage({ stageName, issues, onMoveIssue }) {
     return (
         <div className="mb-8 h-full overflow-y-auto relative  p-1 rounded-lg">
-            <h2 className="text-2xl text-white font-normal mb-4 bg-[#074c5b] tracking-wider sticky top-0 z-10 py-1 text-center font-sans rounded drop-shadow-md tracking-normal" style={style}>
+            <h2 className="text-2xl text-white font-normal mb-4 bg-[#074c5b] tracking-wider sticky top-0 z-10 py-0 text-center font-sans rounded drop-shadow-md tracking-normal " style={style}>
                 {stageName}
             </h2>
             <div className="flex flex-col pt-1 shadow-md rounded-lg h-[650px] lg:h-[500px] sm:h-[480px] xl:h-[700px] md:h-[600px] 2xl:h-[1000px] " style={{   overflowY: 'auto', scrollbarWidth: 'thin', scrollbarColor: 'rgba(0,0,0,0) rgba(0,0,0,0)' }}>
