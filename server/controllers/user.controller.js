cff519ec2aef9e198d03e1c87eae82d8a354f424
Patel-Het03 <hetrpatel03@gmail.com>
2024-03-19 01:23:43 +0530

    intialize workspace.controller.js and workspace.routes.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -13,8 +13,8 @@ const tarnsporter = nodemailer.createTransport({
   requireTLS: true,
   auth:
   {
-      user: process.env.USER,
-      pass: process.env.PASSWORD
+      user: "hetrpatel03@gmail.com",
+      pass: "Rasik@44Patel"
   }
 })
 
@@ -203,7 +203,7 @@ exports.changeinfo = async (req, res) => {
   let password=req.body.newPassword;
   console.log(password)
   
-  const client=new MongoClient('mongodb+srv://Kushagra_18:bDFTOPZeMaygXAE3@cluster0.mxxglsz.mongodb.net/Demo?retryWrites=true&w=majority')
+  const client=new MongoClient('mongodb://127.0.0.1:27017/SE_Project')
 
   try {
   //   if (!password || !email) {
