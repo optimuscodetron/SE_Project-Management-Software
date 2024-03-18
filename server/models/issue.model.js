9a90546c088e308b12bd878d30d52c82d40f8a5e
Kushagra Sharma <96820836+Kushagrasharma18@users.noreply.github.com>
2024-03-18 17:51:19 +0530

    Created Database model
    Created Schema for users,cycles,project,issues and worskpace

---
+++ server/models/issue.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+ 
+const issueSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:String,
+    assigneeUserID :{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    creator:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    stage:{
+        type:String,
+        enum:['Backlog','Todo','Inprogress','Done','Cancelled'],
+        required:true
+    },
+    label:String,
+    priority:{
+        type:String,
+        enum:['Lowest','Low','High','Highest'],
+        required:true
+    },
+    cycleId:{
+     type:Schema.Types.ObjectId,
+     ref:'Cycle'
+    },
+    dueDate:Date,
+    projectId:{
+        type:Schema.Types.ObjectId,
+        ref:'Project',
+        required:true
+    },
+    creationDate:{
+    type:Date,
+    required:true
+    }
+
+},{timestamps: true})
+module.exports.Issue = mongoose.model('Issue', issueSchema);
---
+++ server/models/workspace.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+// const {User}= require("../models/user.model");
+// const { default: Workspace } = require("../../client/src/Workspace");
+
+
+const userworkspaceSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    url:{
+        type:String,
+        required:true
+    },
+    adminuserId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    members:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User',
+            required :true
+        }
+    ],
+    projects: [
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Project',
+            required :true
+        }
+    ]
+
+});
+
+
+// user otp model
+const Workspace = new mongoose.model("Workspace",userworkspaceSchema);
+
+module.exports = Workspace
