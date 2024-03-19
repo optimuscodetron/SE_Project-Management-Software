d224839d2946a37007d9d9f4c6c6f4fed8c7539d
Khushboo <kgupta8646@gmail.com>
2024-03-19 18:12:40 +0530

    Modal Design
    Made modal for invite link pop-up

--- client/src/Workspace/workspace.jsx
+++ client/src/Workspace/workspace.jsx
@@ -3,23 +3,37 @@ import WorkspaceSidebar from "./components/workspaceSidebar/workspaceSidebar";
 import Navbar from "../Components/Layout/navbar/navbar";
 import { useState } from "react";
 import WorkspaceIssues from "./workspaceIssues/workspaceIssues";
+import Invite from "../Invite/Invite";
+
 export default function Workspace() {
   const [showSideBar, setShowSideBar] = useState(false);
   const showSideBarHandler = () => {
     setShowSideBar((prevState) => !prevState);
   };
 
+  const [openInviteMembers , setOpenInviteMembers] = useState(false);
+
+  const openInviteMembersHandler=() => {
+      setOpenInviteMembers(true)
+  }
+  const closeInviteMembersHandler=() => {
+    setOpenInviteMembers(false)
+}
+
   return (
     <div className="flex flex-col">
       <Navbar showSideBarHandler={showSideBarHandler} />
       <div className="flex flex-row">
         <div className="w-fit">
-        <WorkspaceSidebar showSideBar={showSideBar} />
+        <WorkspaceSidebar onOpenInviteMembers={openInviteMembersHandler} showSideBar={showSideBar} />
         </div>
         {/* Main content goes here */}
         <div className="overflow-x-scroll ">
           <WorkspaceIssues/>
         </div>
+        <div className="flex-1">
+        {openInviteMembers  && <Invite onCloseInviteMembers={closeInviteMembersHandler}/>}
+      </div>
       </div>
     </div>
   );
