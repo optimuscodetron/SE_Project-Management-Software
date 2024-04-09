525044ca9906de7c9640c7e1f32f90f2f9d2ef93
Patel-Het03 <hetrpatel03@gmail.com>
2024-04-10 00:11:58 +0530

    added redux persist and call api to fetch all issue of project

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,combineReducers } from '@reduxjs/toolkit'
 import workspaceNameIdReducer from './WorkspaceData/WorkspaceNameIdSlice'
 import activeProjectReducer from './ProjectData/activeProjectSlice'
 import userIdReducer from './userId/userIdSlice' // import the new reducer
 
-export const store = configureStore({
-  reducer: {
-    workspaceNameId: workspaceNameIdReducer,
-    activeProject: activeProjectReducer,
-    userId: userIdReducer,
-  },
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const rootReducer = combineReducers({ 
+  workspaceNameId: workspaceNameIdReducer,
+  activeProject: activeProjectReducer,
+  userId: userIdReducer,
 })
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer
+})
+
+
+export const persistor = persistStore(store)
