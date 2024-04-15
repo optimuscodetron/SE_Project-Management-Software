73bf0f514945489a34754be826c1b1d465c842f4
Kushagra Sharma <96820836+Kushagrasharma18@users.noreply.github.com>
2024-04-16 00:38:29 +0530

    Some minor changes in variable names and database

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -206,9 +206,7 @@ exports.changeinfo = async (req, res) => {
   console.log(password)
   
 
-
-  const client=new MongoClient("mongodb://127.0.0.1:27017/ragnar_SE")
-
+  const client=new MongoClient("mongodb+srv://Kushagra_18:bDFTOPZeMaygXAE3@cluster0.mxxglsz.mongodb.net/Demo?retryWrites=true&w=majority")
 
 
   try {
