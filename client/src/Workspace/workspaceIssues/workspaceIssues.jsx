771358106177445cf21422e9a05af8224f5db607
Piyush Kumar <kumarchspiyush@gmail.com>
2024-04-17 20:26:35 +0530

    resolve errors in redux and patch function

--- client/src/Workspace/workspaceIssues/workspaceIssues.jsx
+++ client/src/Workspace/workspaceIssues/workspaceIssues.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import { NavLink,useNavigate } from "react-router-dom";
-// import IssuesList from "./components/issuesList";
+import IssuesList from "./components/issuesList";
 import IssuePanel from "./components/issuePanel";
 import { LuCircleDashed } from "react-icons/lu";
 import { FaRegCircle } from "react-icons/fa6";
@@ -10,13 +9,11 @@ import { FaRegCheckCircle } from "react-icons/fa";
 import CreateNewProject from "../CreateNewProject/CreateNewProject";
 import FilterSidebar from "./components/FilterSidebar";
 
+
 import { useSelector, useDispatch } from "react-redux";
-// import { Axios } from "axios";
 import axios from "axios";
-import IssuesList from "./components/issuesList";
 
 const WorkspaceIssues = (props) => {
-  const navigate = useNavigate();
   const [toDoIssues, setToDoIssues] = useState([]);
   const [inProgressIssues, setInProgressIssues] = useState([]);
   const [backlogIssues, setBacklogIssues] = useState([]);
@@ -30,254 +27,241 @@ const WorkspaceIssues = (props) => {
   const [selectedAssignee, setSelectedAssignee] = useState(null);
   const [selectedPriority, setSelectedPriority] = useState(null);
   const [selectedProject, setSelectedProject] = useState(null);
+  const [ changeStatusVar, setChangeStatusVar] = useState(false);
 
-  // const [isopen, setIsOpen] = useState(false);
-  // const workspaceId = useSelector((state) => state.workspaceNameId.value.id);
+ const moveIssue = (issueId, currentStatus, newStatus) => {
+    console.log("Function moveIssue Called with status", { newStatus });
+    console.log(issueId);
+    updateIssueStatus(issueId, newStatus);
+
+  };
   console.log(workspaceId);
-
-  const [name, setname] = useState("Ayush");
   
+ 
+  useEffect(() => {
+    if (workspaceId) {
+      const fetchIssues = async () => {
+        try {
+          const response = await axios.get(
+            `http://localhost:8000/api/users/workspace/issues`,
+            {
+              params: {
+                activeWorkspaceId: workspaceId,
+              },
+              withCredentials: true,
+            }
+          );
+          const data = response.data;
+          const modifiedIssues = data.map(issue => {
+            // Extract the last four characters from the ID string
+            const lastFourDigits = issue._id.slice(-4);
+            const projectname=issue.projectname;
+            // console.log(projectname);
+    
+            // Return the issue object with the modified ID
+            return { ...issue, id: lastFourDigits,projectname:projectname };
+          });
+          setIssueList(modifiedIssues);
+          setFilteredList(modifiedIssues);
+
+
+  
+ 
+
+          const todoDummy = [];
+          const inProgressDummy = [];
+          const backlogDummy = [];
+          const doneDummy = [];
+          const cancelledDummy = [];
+
+
+
+          modifiedIssues.forEach((issue) => {
+            if (issue.stage === "ToDo") {
+              todoDummy.push(issue);
+            } else if (issue.stage === "InProgress") {
+              inProgressDummy.push(issue);
+            } else if (issue.stage === "Backlog") {
+              backlogDummy.push(issue);
+            } else if (issue.stage === "Cancelled") {
+              cancelledDummy.push(issue);
+            } else if (issue.stage === "Done") {
+              doneDummy.push(issue);
+            }
+          });
+
+          setToDoIssues(todoDummy);
+          setInProgressIssues(inProgressDummy);
+          setBacklogIssues(backlogDummy);
+          setCancelledIssues(cancelledDummy);
+          setDoneIssues(doneDummy);
+          setDataLoaded(true);
+        } catch (error) {
+          console.error("Error fetching Issues:", error);
+          // Handle errors as needed
+        }
+      };
+
+      fetchIssues();
+    }
+  }, [workspaceId,changeStatusVar]);
+  console.log(IssuesList)
+
 
   const handleFilterAssignee = (name) => {
-    setFilteredList(
-      IssuesList.filter((member, idx) => {
-        return member.assignee.toLowerCase() === name.toLowerCase();
-      })
-    );
-    //  console.log(IssuesList);
-    //  console.log(filteredList);
-    // console.log(name);
+    setSelectedAssignee(name);
   };
 
   const handleFilterPriority = (priority) => {
-    // console.log(IssuesList[0].priority);
-    setFilteredList(
-      IssuesList.filter((member, idx) => {
-        return member?.priority?.toLowerCase() === priority?.toLowerCase();
-      })
-    );
+    setSelectedPriority(priority);
   };
 
   const handleFilterProject = (projectid) => {
-    // console.log(IssuesList[0].priority);
-    setFilteredList(
-      IssuesList.filter((member, idx) => {
-        return member?.projectid === projectid;
-      })
-    );
+    setSelectedProject(projectid);
   };
-
   const handleClear = () => {
-    setFilteredList(IssuesList);
+    setSelectedAssignee(null);
+    setSelectedPriority(null);
+    setSelectedProject(null);
   };
+  
 
   useEffect(() => {
-    const fetchIssues = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:8000/api/users/workspace/issues`,
-          {
-            params: {
-              activeWorkspaceId: workspaceId,
-            },
-            withCredentials: true,
-          }
+    if (IssuesList.length > 0) {
+      const todoDummy = [];
+      const inProgressDummy = [];
+      const backlogDummy = [];
+      const doneDummy = [];
+      const cancelledDummy = [];
+
+      let filteredList = IssuesList;
+      if (selectedAssignee) {
+        filteredList = filteredList.filter(
+          (issue) => issue.assignee.toLowerCase() === selectedAssignee.toLowerCase()
         );
-        const data = response.data;
-        console.log(data);
 
-        const todoDummy = [];
-        const inProgressDummy = [];
-        const backlogDummy = [];
-        const doneDummy = [];
-        const cancelledDummy = [];
+      }
+      if (selectedPriority) {
+        filteredList = filteredList.filter(
+          (issue) =>
+            issue?.priority?.toLowerCase() === selectedPriority.toLowerCase()
+        );
+      }
+      if (selectedProject) {
+        filteredList = filteredList.filter(
+          (issue) => issue?.projectname === selectedProject
+        );
+      }
 
-        data.forEach((issue) => {
-          console.log(issue);
-          if (issue.stage === "ToDo") {
+      setFilteredList(filteredList);
+
+      filteredList.forEach((issue) => {
+        switch (issue.stage) {
+          case "ToDo":
             todoDummy.push(issue);
-          } else if (issue.stage === "InProgress") {
+            break;
+          case "InProgress":
+
             inProgressDummy.push(issue);
-          } else if (issue.stage === "Backlog") {
+            break;
+          case "Backlog":
             backlogDummy.push(issue);
-          } else if (issue.stage === "Cancelled") {
+            break;
+          case "Cancelled":
             cancelledDummy.push(issue);
-          } else if (issue.stage === "Done") {
+            break;
+          case "Done":
             doneDummy.push(issue);
-          }
-        });
-        console.log(todoDummy);
 
-        setToDoIssues(todoDummy);
-        console.log(toDoIssues);
-        setInProgressIssues(inProgressDummy);
-        setBacklogIssues(backlogDummy);
-        setCancelledIssues(cancelledDummy);
-        setDoneIssues(doneDummy);
-        setDataLoaded(true);
-      } catch (error) {
-        console.log("in");
-   fetchDummyIssues();
-        console.error("Error fetching Issues:", error);
-        // Handle errors as needed
-      }
-    };
+            break;
+          default:
+            break;
+        }
+      });
 
-    fetchIssues();
-  }, []);
 
-  const moveIssue = (issueId, currentstage, newstage) => {
-    console.log("Function moveIssue Called with stage", { newstage });
-    let currentIssue;
-    if (currentstage === newstage) {
-      console.log("Current stage and New stage are the same");
-      return 0;
-    } else {
-      const removeFromStage = (issues, setter) => {
-        currentIssue = issues.find((issue) => issue.id === issueId);
-        console.log(currentIssue);
-        currentIssue.stage = newstage;
-        const updatedIssues = issues.filter((issue) => issue.id !== issueId);
-        setter(updatedIssues);
-      };
-      const addToStage = (issues, setter) => {
-        setter([...issues, currentIssue]);
-      };
-      switch (newstage) {
-        case "Backlog":
-          if (currentstage === "ToDo")
-            removeFromStage(toDoIssues, setToDoIssues);
-          else if (currentstage === "InProgress")
-            removeFromStage(inProgressIssues, setInProgressIssues);
-          else if (currentstage === "Done")
-            removeFromStage(doneIssues, setDoneIssues);
-          else if (currentstage === "Cancelled")
-            removeFromStage(cancelledIssues, setCancelledIssues);
-          // removeFromStage(Issues, setter);
-          addToStage(backlogIssues, setBacklogIssues);
-          break;
-        case "ToDo":
-          if (currentstage === "Backlog")
-            removeFromStage(backlogIssues, setBacklogIssues);
-          else if (currentstage === "InProgress")
-            removeFromStage(inProgressIssues, setInProgressIssues);
-          else if (currentstage === "Done")
-            removeFromStage(doneIssues, setDoneIssues);
-          else if (currentstage === "Cancelled")
-            removeFromStage(cancelledIssues, setCancelledIssues);
-          addToStage(toDoIssues, setToDoIssues);
-          break;
-        case "InProgress":
-          if (currentstage === "Backlog")
-            removeFromStage(backlogIssues, setBacklogIssues);
-          else if (currentstage === "ToDo")
-            removeFromStage(toDoIssues, setToDoIssues);
-          else if (currentstage === "Done")
-            removeFromStage(doneIssues, setDoneIssues);
-          else if (currentstage === "Cancelled")
-            removeFromStage(cancelledIssues, setCancelledIssues);
-          addToStage(inProgressIssues, setInProgressIssues);
-          break;
-        case "Done":
-          if (currentstage === "Backlog")
-            removeFromStage(backlogIssues, setBacklogIssues);
-          else if (currentstage === "ToDo")
-            removeFromStage(toDoIssues, setToDoIssues);
-          else if (currentstage === "InProgress")
-            removeFromStage(inProgressIssues, setInProgressIssues);
-          else if (currentstage === "Cancelled")
-            removeFromStage(cancelledIssues, setCancelledIssues);
-          addToStage(doneIssues, setDoneIssues);
-          break;
-        case "Cancelled":
-          if (currentstage === "Backlog")
-            removeFromStage(backlogIssues, setBacklogIssues);
-          else if (currentstage === "ToDo")
-            removeFromStage(toDoIssues, setToDoIssues);
-          else if (currentstage === "InProgress")
-            removeFromStage(inProgressIssues, setInProgressIssues);
-          else if (currentstage === "Done")
-            removeFromStage(doneIssues, setDoneIssues);
-          addToStage(cancelledIssues, setCancelledIssues);
-          break;
-        default:
-          break;
-      }
+      setToDoIssues(todoDummy);
+      setInProgressIssues(inProgressDummy);
+      setBacklogIssues(backlogDummy);
+      setCancelledIssues(cancelledDummy);
+      setDoneIssues(doneDummy);
+      setDataLoaded(true);
     }
-  };
-  const fn = ()=>{
-    // navigate("/workspace/project/board/issue");
-    console.log("naviagte");
+  }, [IssuesList, selectedAssignee, selectedPriority, selectedProject]);
+
+  const updateIssueStatus = async (issueId, newStatus) => {
+    try {
+      // Send a PATCH request to update the issue status
+      const response = await axios.patch(`http://localhost:8000/issues/${issueId}/changeStatus`, { newStatus });
+      // Handle the response
+      if (response.status === 200) {
+        console.log(response.data);
+        setChangeStatusVar(previousValue => !previousValue);
+      }
+    } catch (error) {
+      // Handle errors
+      console.error('Error updating issue status:', error);
+    }
   }
 
+
   return (
+    
     <div className="bg-[#171e28] overflow-x-scroll px-2 pt-2">
       {dataLoaded && (
         <div className="flex flex-row w-screen">
-         
-            <div className="w-[320px] mx-1">
-              <IssuePanel
-                stageName="Backlog"
-                issues={backlogIssues}
-                onMoveIssue={moveIssue}
-                icon={<LuCircleDashed />}
-               
-                />
-             
-            </div>
-          
-            <div className="w-[320px] mx-1">
-              <IssuePanel
-                stageName="To Do"
-                issues={toDoIssues}
-                onMoveIssue={moveIssue}
-                icon={<FaRegCircle />}
-                onClick={fn}
-              />
-            </div>
-           
-            <div className="w-[320px] mx-1">
-              <IssuePanel
-                stageName="In Progress"
-                issues={inProgressIssues}
-                onMoveIssue={moveIssue}
-                icon={<FaCircleHalfStroke />}
-                iconColor="text-yellow-400"
-                onClick={fn}
-              />
-            </div>
-           
-            <div className="w-[320px] mx-1">
-              <IssuePanel
-                stageName="Done"
-                issues={doneIssues}
-                onMoveIssue={moveIssue}
-                icon={<FaRegCheckCircle />}
-                iconColor="text-green-400"
-              />
-            </div>
-          
-            <NavLink to="/workspace/project/board/issue">
-            <div className="w-[320px] mx-1">
-              <IssuePanel
-                stageName="Cancelled"
-                issues={cancelledIssues}
-                onMoveIssue={moveIssue}
-                icon={<FaRegTimesCircle />}
-                iconColor="text-red-400"
-              />
-            </div>
-          </NavLink>
+          <div className="w-[320px] mx-1">
+            <IssuePanel
+              stageName="Backlog"
+              issues={backlogIssues}
+              onMoveIssue={moveIssue}
+              icon={<LuCircleDashed />}
+            />
+          </div>
+          <div className="w-[320px] mx-1">
+            <IssuePanel
+              stageName="To Do"
+              issues={toDoIssues}
+              onMoveIssue={moveIssue}
+              icon={<FaRegCircle />}
+            />
+          </div>
+          <div className="w-[320px] mx-1">
+            <IssuePanel
+              stageName="In Progress"
+              issues={inProgressIssues}
+              onMoveIssue={moveIssue}
+              icon={<FaCircleHalfStroke />}
+              iconColor="text-yellow-400"
+            />
+          </div>
+          <div className="w-[320px] mx-1">
+            <IssuePanel
+              stageName="Done"
+              issues={doneIssues}
+              onMoveIssue={moveIssue}
+              icon={<FaRegCheckCircle />}
+              iconColor="text-green-400"
+            />
+          </div>
+          <div className="w-[320px] mx-1">
+            <IssuePanel
+              stageName="Cancelled"
+              issues={cancelledIssues}
+              onMoveIssue={moveIssue}
+              icon={<FaRegTimesCircle />}
+              iconColor="text-red-400"
+            />
+          </div>
           {props.showFilterSidebar && (
             <div className="overflow-y-scroll">
-              <div className=" fixed right-0 h-full overflow-y-scroll">
+              <div className="fixed right-0 h-full overflow-y-scroll">
                 <FilterSidebar
                   handleFilterAssignee={handleFilterAssignee}
                   handleClear={handleClear}
                   handleFilterPriority={handleFilterPriority}
                   handleFilterProject={handleFilterProject}
                 />
-                {/* <FilterSidebar /> */}
               </div>
             </div>
           )}
@@ -286,4 +270,5 @@ const WorkspaceIssues = (props) => {
     </div>
   );
 };
+
 export default WorkspaceIssues;
--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,21 +1,18 @@
-const mongoose = require('mongoose');
-const { Workspace } = require('../models/workspace.model');
+const mongoose = require("mongoose");
+const { Workspace } = require("../models/workspace.model");
 // const db=process.env.DB
 
-
-
-
-
-mongoose.connect("mongodb://127.0.0.1:27017/ragnar_SE", {
-
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-})
-    .then(() => {
-        console.log("Established connection to the database")
-
-})
-    .catch(err => console.log("Something went wrong ", err));
-
+mongoose
+  .connect(
+    "mongodb+srv://Kushagra_18:bDFTOPZeMaygXAE3@cluster0.mxxglsz.mongodb.net/Demo?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("Established connection to the database");
+  })
+  .catch((err) => console.log("Something went wrong ", err));
